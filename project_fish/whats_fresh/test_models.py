138d9e6957067e015b2040177587b25313c8b06e
Evan Tschuy <tschuy@osuosl.org>
2014-08-01 09:08:57 -0700

    Update Vendor model with another test
    Made the 'only contain what I want' test, and made the other tests
    actually work.

--- project_fish/whats_fresh/test_models.py
+++ project_fish/whats_fresh/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.conf import settings
-from django.db import models
 
-from whats_fresh.models import Image
+from whats_fresh.models import *
+from django.contrib.gis.db import models
 
 import os
 import time
@@ -23,20 +23,28 @@ class ImageTestCase(TestCase):
         self.creation_time = datetime.datetime.now()
         self.image.save()
 
-    def fields_exist(self):
+    def test_fields_exist(self):
         model = models.get_model('whats_fresh', 'Image')
         self.assertEqual(
             models.ImageField,
-            type(model._meta.get_field_by_name('image')))
+            type(model._meta.get_field_by_name('image')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('caption')))
+            type(model._meta.get_field_by_name('caption')[0]))
         self.assertEqual(
             models.DateTimeField,
-            type(model._meta.get_field_by_name('created')))
+            type(model._meta.get_field_by_name('created')[0]))
         self.assertEqual(
             models.DateTimeField,
-            type(model._meta.get_field_by_name('modified')))
+            type(model._meta.get_field_by_name('modified')[0]))
+
+    def test_no_additional_fields(self):
+        fields = Image._meta.get_all_field_names()
+        expected_fields = ['image', 'caption', 'created', 'modified', u'id']
+
+        self.assertTrue(sorted(fields) == sorted(expected_fields))
+
+
 
     def tearDown(self):
         # reset MEDIA_ROOT
@@ -79,47 +87,49 @@ class VendorTestCase(TestCase):
         model = models.get_model('whats_fresh', 'Vendor')
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('name')))
+            type(model._meta.get_field_by_name('name')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('description')))
+            type(model._meta.get_field_by_name('description')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('street')))
+            type(model._meta.get_field_by_name('street')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('city')))
+            type(model._meta.get_field_by_name('city')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('state')))
+            type(model._meta.get_field_by_name('state')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('zip')))
+            type(model._meta.get_field_by_name('zip')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('location_description')))
+            type(model._meta.get_field_by_name('location_description')[0]))
         self.assertEqual(
             models.TextField,
-            type(model._meta.get_field_by_name('contact_name')))
-        self.assertEqual(
-            models.TextField,
-            type(model._meta.get_field_by_name('contact_name')))
-        self.assertEqual(
-            models.TextField,
-            type(model._meta.get_field_by_name('contact_name')))
+            type(model._meta.get_field_by_name('contact_name')[0]))
         self.assertEqual(
             models.FloatField,
-            type(model._meta.get_field_by_name('lat')))
+            type(model._meta.get_field_by_name('lat')[0]))
         self.assertEqual(
             models.FloatField,
-            type(model._meta.get_field_by_name('long')))
+            type(model._meta.get_field_by_name('long')[0]))
         self.assertEqual(
             models.URLField,
-            type(model._meta.get_field_by_name('website')))
+            type(model._meta.get_field_by_name('website')[0]))
         self.assertEqual(
             models.EmailField,
-            type(model._meta.get_field_by_name('email')))
+            type(model._meta.get_field_by_name('email')[0]))
         self.assertEqual(
             PhoneNumberField,
-            type(model._meta.get_field_by_name('phone')))
+            type(model._meta.get_field_by_name('phone')[0]))
+
+    def no_additional_fields(self):
+        fields = Vendor._meta.get_all_field_names()
+        expected_fields = ['name', 'description', 'street', 'city', 'state',
+                           'zip', 'location_description', 'contact_name',
+                           'lat', 'long', 'website', 'email', 'phone', u'id']
+
+        self.assertTrue(sorted(fields) == sorted(expected_fields))
 
