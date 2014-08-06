2170c1498257f977e8eaff6e7194fd076785eb8c
MaraJade <catbookgirl@gmail.com>
2014-08-06 10:23:26 -0700

    Renamed everything from project_fish to whats_fresh
    refs #16449
    
    Hopefully this will fix the merging problem

---
+++ whats_fresh/whats_fresh_api/models.py
@@ -0,0 +1,32 @@
+from django.contrib.gis.db import models
+import os
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Image(models.Model):
+    """
+    The Image model holds an image and related data.
+
+    The Created and Modified time fields are created automatically by
+    Django when the object is created or modified, and can not be altered.
+
+    This model uses Django's built-ins for holding the image location and
+    data in the database, as well as for keeping created and modified
+    timestamps.
+    """
+
+    image = models.ImageField(upload_to='images')
+    caption = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Vendor(models.Model):
+    """
+    The Vendor model holds the information for a vendor, including the
+    geographic location as a pair of latitudinal/logitudinal coordinates,
+    a street address, and an optional text description of their location
+    (in case the address/coordinates are of, say, a dock instead of a shop).
+    """
+    pass
---
+++ whats_fresh/whats_fresh_api/tests/test_image_model.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.files import File
+
+from whats_fresh.models import *
+from django.contrib.gis.db import models
+
+import os
+import time
+import sys
+import datetime
+
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        self.expected_fields = {
+            'image': models.ImageField,
+            'caption': models.TextField,
+            'created': models.DateTimeField,
+            'modified': models.DateTimeField,
+            'id': models.AutoField
+        }
+
+    def test_fields_exist(self):
+        model = models.get_model('whats_fresh', 'Image')
+        for field, field_type in self.expected_fields.items():
+            self.assertEqual(
+                field_type, type(model._meta.get_field_by_name(field)[0]))
+
+    def test_parameters(self):
+        self.assertEqual(
+            Image._meta.get_field_by_name('image')[0].upload_to,
+            'images')
+
+    def test_no_additional_fields(self):
+        fields = Image._meta.get_all_field_names()
+        self.assertTrue(sorted(fields) == sorted(self.expected_fields.keys()))
+
+    def test_created_modified_fields(self):
+        self.assertTrue(Image._meta.get_field('modified').auto_now)
+        self.assertTrue(Image._meta.get_field('created').auto_now_add)
