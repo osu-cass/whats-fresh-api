9847307573f739cba5da7f7044a89d428cae563d
MaraJade <catbookgirl@gmail.com>
2014-08-06 15:04:57 -0700

    Added many-to-many field in preparation test
    refs #16581

--- whats_fresh/whats_fresh_api/tests/test_preparation_model.py
+++ whats_fresh/whats_fresh_api/tests/test_preparation_model.py
@@ -17,6 +17,7 @@ class PreparationsTestCase(TestCase):
             'name': models.TextField,
             'description': models.TextField,
             'additional_info': models.TextField,
+            'product': models.ManyToManyField,
             'id': models.AutoField
         }
 
