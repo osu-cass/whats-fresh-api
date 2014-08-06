50808b1d2a72fd57aa1e9c612f8d0e8bb16ec97f
Evan Tschuy <tschuy@osuosl.org>
2014-08-06 16:10:15 -0700

    Update and move VendorProduct test

--- whats_fresh/whats_fresh_api/tests/test_vendor_product_join_model.py
+++ whats_fresh/whats_fresh_api/tests/test_vendor_product_join_model.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.conf import settings
 from phonenumber_field.modelfields import PhoneNumberField
 
-from whats_fresh.models import *
+from whats_fresh_api.models import *
 from django.contrib.gis.db import models
 
 
@@ -26,3 +26,9 @@ class VendorProductJoinTestCase(TestCase):
     def test_no_additional_fields(self):
         fields = VendorProduct._meta.get_all_field_names()
         self.assertTrue(sorted(fields) == sorted(self.expected_fields.keys()))
+
+    def test___unicode___method(self):
+        try:
+            result = VendorProduct.__unicode__(VendorProduct())
+        except AttributeError as e:
+            self.fail("No __unicode__ method found")
