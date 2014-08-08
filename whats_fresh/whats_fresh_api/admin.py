54309c0950a475471c19fd0e03d2bc5b88521704
Evan Tschuy <tschuy@osuosl.org>
2014-08-08 11:37:09 -0700

    Create basic admin interface

--- whats_fresh/whats_fresh_api/admin.py
+++ whats_fresh/whats_fresh_api/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from whats_fresh_api.models import Image
+from whats_fresh_api.models import *
+
+class VendorProductInline(admin.TabularInline):
+    model = VendorProduct
+    extra = 2
 
 class ImageAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified')
+class VendorAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    inlines = (VendorProductInline,)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    inlines = (VendorProductInline,)
 
 admin.site.register(Image, ImageAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(Preparation)
+admin.site.register(Story)
