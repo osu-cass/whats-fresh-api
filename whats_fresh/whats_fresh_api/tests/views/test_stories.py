51231df10107ef5e7c125e82c2bd9f7f8d4f89c5
Evan Tschuy <tschuy@osuosl.org>
2014-08-11 16:08:15 -0700

    Fix typo, pep8 issues

--- whats_fresh/whats_fresh_api/tests/views/test_stories.py
+++ whats_fresh/whats_fresh_api/tests/views/test_stories.py
@@ -10,9 +10,8 @@ class StoriesTestCase(TestCase):
     fixtures = ['whats_fresh_api/tests/testdata/test_fixtures.json']
 
     def setUp(self):
-        self.expected_json = """"
+        self.expected_json = """
 {
-<<<<<<< HEAD
     "error": {
         "error_status": false,
         "error_name": null,
@@ -22,10 +21,9 @@ class StoriesTestCase(TestCase):
     "story": "These are the voyages of the Starfish Enterblub; her five year mission -- to seek out new fish and new fishilizations..."
 }"""
 
-
     def test_url_endpoint(self):
-       url = reverse('story-details', kwargs={'id': '1'})
-       self.assertEqual(url, '/stories/1')
+        url = reverse('story-details', kwargs={'id': '1'})
+        self.assertEqual(url, '/stories/1')
 
     def test_json_equals(self):
         c = Client()
