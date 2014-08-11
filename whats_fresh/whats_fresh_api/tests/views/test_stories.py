246105d5d0bb5640fab4f359c18c899edba65975
Evan Tschuy <tschuy@osuosl.org>
2014-08-11 13:29:24 -0700

    Write /stories/<id> view test

---
+++ whats_fresh/whats_fresh_api/tests/views/test_stories.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from whats_fresh_api.models import *
+from django.contrib.gis.db import models
+import json
+
+
+class StoriesTestCase(TestCase):
+    fixtures = ['whats_fresh_api/tests/testdata/test_fixtures.json']
+
+    def setUp(self):
+        self.expected_json = """
+{
+    "story": "These are the voyages of the Starfish Enterblub; her five year mission -- to seek out new fish and new fishilizations..."
+}"""
+
+    def test_url_endpoint(self):
+       url = reverse('story-details', kwargs={'id': '1'})
+       self.assertEqual(url, '/stories/1')
+
+    def test_json_equals(self):
+        c = Client()
+        response = c.get(reverse('story-details', kwargs={'id': '1'})).content
+        parsed_answer = json.loads(response)
+
+        expected_answer = json.loads(self.expected_json)
+        self.assertTrue(parsed_answer == expected_answer)
