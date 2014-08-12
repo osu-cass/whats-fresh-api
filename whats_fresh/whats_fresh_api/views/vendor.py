e0da803e0f410a0e4c8cd7eafdda5c2fba9e6344
Evan Tschuy <tschuy@osuosl.org>
2014-08-11 17:13:01 -0700

    Added HttpResponseServerError impot

--- whats_fresh/whats_fresh_api/views/vendor.py
+++ whats_fresh/whats_fresh_api/views/vendor.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import (HttpResponse,
+                         HttpResponseNotFound,
+                         HttpResponseServerError)
 from whats_fresh_api.models import Vendor, Product, VendorProduct
 from django.forms.models import model_to_dict
 import json
