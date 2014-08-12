25ac1c6f7b591e10acba7abb4539098a1a71ec8b
Evan Tschuy <tschuy@osuosl.org>
2014-08-12 11:27:43 -0700

    Write story view

--- whats_fresh/whats_fresh_api/urls.py
+++ whats_fresh/whats_fresh_api/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^vendors/?$', 'whats_fresh_api.views.vendor.vendor_list', name='vendors-list'),
+    url(r'^stories/(?P<id>\d+)/?$', 'whats_fresh_api.views.story.story_details', name='story-details'),
 )
+
---
+++ whats_fresh/whats_fresh_api/views/story.py
@@ -0,0 +1,47 @@
+from django.http import (HttpResponse,
+                         HttpResponseNotFound,
+                         HttpResponseServerError)
+from whats_fresh_api.models import Story
+from django.forms.models import model_to_dict
+import json
+
+
+def story_details(request, id=None):
+    data = {}
+
+    try:
+        story = Story.objects.get(id=id)
+    except Exception as e:
+        data['error'] = {
+            'error_status': True,
+            'error_level': 'Important',
+            'error_text': 'Story with id %s not found!' % id,
+            'error_name': 'Story Not Found'
+        }
+        return HttpResponseNotFound(
+            json.dumps(data),
+            content_type="application/json"
+        )
+        
+    try:
+        data = model_to_dict(story, fields=[], exclude=[])
+        del data['id']
+        data['error'] = {
+            'error_status': False,
+            'error_level': None,
+            'error_text': None,
+            'error_name': None
+        }
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+    except:
+        data['error'] = {
+            'error_status': True,
+            'error_level': 'Severe',
+            'error_text': 'An unknown error occurred processing story %s' % id,
+            'error_name': 'Unknown'
+        }
+        return HttpResponseServerError(
+            json.dumps(data),
+            content_type="application/json"
+        )
