9583bfcbb2c473cd4859950217bf2ca2e113310c
Evan Tschuy <tschuy@osuosl.org>
2014-08-12 11:37:22 -0700

    Write /vendors/products/<id> view

--- whats_fresh/whats_fresh_api/urls.py
+++ whats_fresh/whats_fresh_api/urls.py
@@ -2,8 +2,16 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^vendors/?$', 'whats_fresh_api.views.vendor.vendor_list', name='vendors-list'),
-    url(r'^stories/(?P<id>\d+)/?$', 'whats_fresh_api.views.story.story_details', name='story-details'),
-    url(r'^products/(?P<id>\d+)/?$', 'whats_fresh_api.views.product.product_details', name='product-details'),
+    url(r'^stories/(?P<id>\d+)/?$',
+        'whats_fresh_api.views.story.story_details', name='story-details'),
+    url(r'^products/(?P<id>\d+)/?$',
+        'whats_fresh_api.views.product.product_details',
+        name='product-details'),
+
+    url(r'^vendors/?$', 'whats_fresh_api.views.vendor.vendor_list',
+        name='vendors-list'),
+    url(r'^vendors/products/(?P<id>\d+)/?$',
+        'whats_fresh_api.views.vendor.vendors_products',
+        name='vendors-products'),
 )
 
--- whats_fresh/whats_fresh_api/views/vendor.py
+++ whats_fresh/whats_fresh_api/views/vendor.py
@@ -64,3 +64,71 @@ def vendor_list(request):
             json.dumps(data),
             content_type="application/json"
         )
+
+
+def vendors_products(request, id=None):
+    data = {}
+    try:
+        vendor_list = Vendor.objects.filter(products__id__contains=id)
+    except Exception as e:
+        data['error'] = {
+            'error_status': True,
+            'error_level': 'Severe',
+            'error_text': 'Product id is invalid',
+            'error_name': 'Invalid product'
+        }
+
+    if len(vendor_list) == 0:
+        data['error'] = {
+            'error_status': True,
+            'error_level': 'Important',
+            'error_text': 'Could not find any vendors for product %s!' % id,
+            'error_name': 'No Vendors for product %s' % id
+        }
+        return HttpResponseNotFound(
+            json.dumps(data),
+            content_type="application/json"
+        )
+
+    try:
+        for vendor in vendor_list:
+            data[str(vendor.id)] = model_to_dict(vendor, fields=[], exclude=[])
+            data[str(vendor.id)]['phone'] = data[
+                str(vendor.id)]['phone'].national_number
+
+            data[str(vendor.id)]['created'] = str(vendor.created)
+            data[str(vendor.id)]['updated'] = str(vendor.modified)
+            data[str(vendor.id)]['ext'] = {}
+            del data[str(vendor.id)]['id']
+
+            data[str(vendor.id)]['story'] = data[str(vendor.id)].pop('story_id')
+
+            products = data[str(vendor.id)]['products']
+            data[str(vendor.id)]['products'] = {}
+            for product_id in products:
+                product = VendorProduct.objects.get(id=product_id)
+                product_data = {
+                    'preparation': product.preparation.name,
+                    'name': product.product.name
+                }
+                data[str(vendor.id)]['products'][product_id] = product_data
+
+        data['error'] = {
+            'error_status': False,
+            'error_level': None,
+            'error_text': None,
+            'error_name': None
+        }
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+    except:
+        data['error'] = {
+            'error_status': True,
+            'error_level': 'Severe',
+            'error_text': 'An unknown error occurred processing the vendors for product %s' % id,
+            'error_name': 'Unknown'
+        }
+        return HttpResponseServerError(
+            json.dumps(data),
+            content_type="application/json"
+        )
