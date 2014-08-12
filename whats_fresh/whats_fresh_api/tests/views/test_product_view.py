6cf3d8f361d23f41dc87dc6fe5cf49e4ad8c638f
MaraJade <catbookgirl@gmail.com>
2014-08-12 09:31:55 -0700

    Fixed typos and changed image from an id to a url
    refs #16479
    
    Changed setup to setUp, made the expected json a string, removed extra lines
    from json string.

--- whats_fresh/whats_fresh_api/tests/views/test_product_view.py
+++ whats_fresh/whats_fresh_api/tests/views/test_product_view.py
@@ -17,8 +17,8 @@ class ProductViewTestCase(TestCase):
 
     fixtures = ['whats_fresh_api/tests/testdata/test_fixtures.json']
 
-    def setup(self):
-        self.expected_json = {
+    def setUp(self):
+        self.expected_json = '{
             "error": {
                 "error_status": false,
                 "error_name": null,
@@ -26,44 +26,38 @@ class ProductViewTestCase(TestCase):
                 "error_level": null
             },
             "1": {
-                "model": "whats_fresh_api.Product",
-                "pk": "2",
-                    "fields": {
-                    "name": "Starfish Voyager",
-                    "variety": "Tuna",
-                    "alt_name": "The Stargazer",
-                    "description": "This is one sweet fish!",
-                    "origin": "The Delta Quadrant",
-                    "season": "Season 1",
-                    "available": true,
-                    "market_price": "$33.31",
-                    "link": "http://www.amazon.com/Star-Trek-Voyager-Complete-Seventh/dp/B00062IDCO/",
-                    "image_id": 1,
-                    "story_id": 1,
-                    "created": "2014-08-08 23:27:05.568395+00:00",
-                    "modified": "2014-08-08 23:27:05.568395+00:00"
+                "name": "Starfish Voyager",
+                "variety": "Tuna",
+                "alt_name": "The Stargazer",
+                "description": "This is one sweet fish!",
+                "origin": "The Delta Quadrant",
+                "season": "Season 1",
+                "available": true,
+                "market_price": "$33.31",
+                "link": "http://www.amazon.com/Star-Trek-Voyager-Complete-Seventh/dp/B00062IDCO/",
+                "image": "/media/dog.jpg",
+                "story_id": 1,
+                "created": "2014-08-08 23:27:05.568395+00:00",
+                "modified": "2014-08-08 23:27:05.568395+00:00"
                 }
             },
             "2": {
-                "model": "whats_fresh_api.Product",
-                "pk": "1",
-                "fields": {
-                    "name": "Ezri Dax",
-                    "variety": "Freshwater Eel",
-                    "alt_name": "Jadzia",
-                    "description": "That's not actually an eel, it's a symbiote.",
-                    "origin": "Trill",
-                    "season": "Season 7",
-                    "available": true,
-                    "market_price": "$32.64 per season",
-                    "link": "http://www.amazon.com/Star-Trek-Deep-Space-Nine/dp/B00008KA57/",
-                    "image_id": 1,
-                    "story_id": 2,
-                    "created": "2014-08-08 23:27:05.568395+00:00",
-                    "modified": "2014-08-08 23:27:05.568395+00:00"
+                "name": "Ezri Dax",
+                "variety": "Freshwater Eel",
+                "alt_name": "Jadzia",
+                "description": "That's not actually an eel, it's a symbiote.",
+                "origin": "Trill",
+                "season": "Season 7",
+                "available": true,
+                "market_price": "$32.64 per season",
+                "link": "http://www.amazon.com/Star-Trek-Deep-Space-Nine/dp/B00008KA57/",
+                "image_id": "/media/cat.jpg",
+                "story_id": 2,
+                "created": "2014-08-08 23:27:05.568395+00:00",
+                "modified": "2014-08-08 23:27:05.568395+00:00"
                 }
             }
-        }
+        }'
 
     def test_url_endpoint(self):
         url = reverse('products-list')
