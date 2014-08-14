f65142bc9f70b136293b1193bb747c228c622413
MaraJade <catbookgirl@gmail.com>
2014-08-14 13:18:34 -0700

    Fixed the product/vendor test

--- whats_fresh/whats_fresh_api/tests/views/test_product_vendor_details.py
+++ whats_fresh/whats_fresh_api/tests/views/test_product_vendor_details.py
@@ -44,7 +44,7 @@ class ProductVendorTestCase(TestCase):
                 "available": true,
                 "market_price": "$32.64 per season",
                 "link": "http://www.amazon.com/Star-Trek-Deep-Space-Nine/dp/B00008KA57/",
-                "image": "/media/dog.jpg",
+                "image": "/media/cat.jpg",
                 "story_id": 2,
                 "created": "2014-08-08 23:27:05.568395+00:00",
                 "modified": "2014-08-08 23:27:05.568395+00:00"
