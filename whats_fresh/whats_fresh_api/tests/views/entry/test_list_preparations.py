b42d28a2f6fbe936a3211e914a3794a7bcf176bc
Evan Tschuy <tschuy@osuosl.org>
2014-09-04 15:36:37 -0700

    Remove duplicate test_list_items function from test_list_preparations

--- whats_fresh/whats_fresh_api/tests/views/entry/test_list_preparations.py
+++ whats_fresh/whats_fresh_api/tests/views/entry/test_list_preparations.py
@@ -12,23 +12,6 @@ class ListPreparationTestCase(TestCase):
         url = reverse('entry-list-preparations')
         self.assertEqual(url, '/entry/preparations')
 
-    def test_list_items(self):
-        """
-        Tests to see if the list of preparations
-        contains the proper preparations
-        """
-        response = self.client.get(reverse('entry-list-preparations'))
-        items = response.context['item_list']
-
-        for preparation in Preparation.objects.all():
-            self.assertEqual(
-                items[preparation.id-1]['description'], preparation.description)
-            self.assertEqual(
-                items[preparation.id-1]['name'], preparation.name)
-            self.assertEqual(
-                items[preparation.id-1]['link'],
-                reverse('edit-preparation', kwargs={'id': preparation.id}))
-
 
     def test_list_items(self):
         """
