26580bbfdd743c7ea444aa9a1c1d4569cec34b8c
Evan Tschuy <tschuy@osuosl.org>
2014-09-04 16:55:12 -0700

    Make dummy login view dumber

--- whats_fresh/whats_fresh_api/views/data_entry/login.py
+++ whats_fresh/whats_fresh_api/views/data_entry/login.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 
 def login_user(request):
     user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-    login(request, user)
 
-    return HttpResponseRedirect('/')
+    return HttpResponse('')
