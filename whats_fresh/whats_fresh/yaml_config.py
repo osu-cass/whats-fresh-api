078c0108e33c05e85955b414a7949b60160fe4e2
Evan Tschuy <tschuy@osuosl.org>
2014-09-18 14:45:46 -0700

    Use YML to configure whats_fresh_api

---
+++ whats_fresh/whats_fresh/yaml_config.py
@@ -0,0 +1,17 @@
+from yaml import load
+import os
+
+DEFAULT_CONFIG_DIR = '/opt/whats_fresh/config'
+CONFIG_DIR = os.environ.get('WF_CONFIG_DIR', DEFAULT_CONFIG_DIR)
+
+CONFIG_PATH = os.path.join(CONFIG_DIR, 'config.yml')
+
+# Open our yaml config and override settings values with it's config
+try:
+    config_file = open(CONFIG_PATH)
+    for key, value in load(config_file.read()).items():
+        globals()[key] = value
+    config_file.close()
+except Exception, e:
+    print "Could not find, or open config located at %s" % CONFIG_PATH
+    print "The error occured was: %s" % e
