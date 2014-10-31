947ffeb7666627a29f3e1d60d9a9ff61738f9d23
Evan Tschuy <tschuy@osuosl.org>
2014-10-31 14:00:40 -0700

    Move dependencis into setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from distutils.core import setup
-from pip.req import parse_requirements
 from setuptools import find_packages
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
+dependencies = [
+    'Django==1.7',
+    'Jinja2==2.7.3',
+    'MarkupSafe==0.23',
+    'Pillow==2.5.1',
+    'PyYAML==3.11',
+    'Pygments==1.6',
+    'Sphinx==1.2.2',
+    'argparse==1.2.1',
+    'django-phonenumber-field==0.6',
+    'docutils==0.12',
+    'mock==1.0.1',
+    'pep8==1.5.7',
+    'phonenumbers==6.2.0',
+    'psycopg2==2.5.3',
+    'requests==2.3.0',
+    'wsgiref==0.1.2'
+]
 
 setup(
     name='whats-fresh',
     version='0.1.0',
-    install_requires=reqs,
+    install_requires=dependencies,
     author=u'OSU Center for Applied Systems and Software',
     author_email='support@osuosl.org',
     packages=find_packages(),
