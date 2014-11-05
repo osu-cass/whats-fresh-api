23c5dc7eef98d1c4ebdf6611259ad74c649e2d03
Evan Tschuy <tschuy@osuosl.org>
2014-11-04 17:00:31 -0800

    Write video entry tests

---
+++ whats_fresh/whats_fresh_api/tests/views/entry/test_list_videos.py
@@ -0,0 +1,74 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from whats_fresh.whats_fresh_api.models import Video
+from django.contrib.auth.models import User, Group
+
+
+class ListVideoTestCase(TestCase):
+    fixtures = ['thirtythree']
+
+    def setUp(self):
+        user = User.objects.create_user(
+            'temporary', 'temporary@gmail.com', 'temporary')
+        user.save()
+
+        admin_group = Group(name='Administration Users')
+        admin_group.save()
+        user.groups.add(admin_group)
+
+        response = self.client.login(
+            username='temporary', password='temporary')
+        self.assertEqual(response, True)
+
+    def test_not_logged_in(self):
+        self.client.logout()
+
+        response = self.client.get(
+            reverse('edit-video', kwargs={'id': '1'}))
+        self.assertRedirects(response, '/login?next=/entry/videos/1')
+
+    def test_url_endpoint(self):
+        url = reverse('entry-list-videos')
+        self.assertEqual(url, '/entry/videos')
+
+    def test_list_items(self):
+        """
+        Tests to see if the list of videos contains the proper
+        videos and proper video data
+        """
+
+        page_1 = self.client.get(reverse('entry-list-videos')).context
+        page_2 = self.client.get(
+            '{}?page=2'.format(reverse('entry-list-videos'))).context
+        page_3 = self.client.get(
+            '{}?page=3'.format(reverse('entry-list-videos'))).context
+        page_4 = self.client.get(
+            '{}?page=4'.format(reverse('entry-list-videos'))).context
+        page_nan = self.client.get(
+            '{}?page=NaN'.format(reverse('entry-list-videos'))).context
+
+        self.assertEqual(
+            list(page_1['item_list']),
+            list(Video.objects.all()[:15]))
+
+        self.assertEqual(
+            list(page_2['item_list']),
+            list(Video.objects.all()[15:30]))
+
+        self.assertEqual(
+            list(page_3['item_list']),
+            list(Video.objects.all()[30:33]))
+
+        # Page 4 should be identical to Page 3, as these fixtures
+        # have enough content for three pages (15 items per page, 33 items)
+
+        self.assertEqual(
+            list(page_3['item_list']),
+            list(page_4['item_list']))
+
+        # Page NaN should be identical to Page 1, as Django paginator returns
+        # the first page if the page is not an int
+
+        self.assertEqual(
+            list(page_1['item_list']),
+            list(page_nan['item_list']))
---
+++ whats_fresh/whats_fresh_api/tests/views/entry/test_new_video.py
@@ -0,0 +1,112 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from whats_fresh.whats_fresh_api.models import Video
+from django.contrib.auth.models import User, Group
+
+
+class NewVideoTestCase(TestCase):
+
+    """
+    Test that the New Video page works as expected.
+
+    Things tested:
+        URLs reverse correctly
+        The outputted page has the correct form fields
+        POSTing "correct" data will result in the creation of a new
+            object with the specified details
+        POSTing data with all fields missing (hitting "save" without entering
+            data) returns the same field with notations of missing fields
+    """
+
+    def setUp(self):
+        user = User.objects.create_user(
+            'temporary', 'temporary@gmail.com', 'temporary')
+        user.save()
+
+        admin_group = Group(name='Administration Users')
+        admin_group.save()
+        user.groups.add(admin_group)
+
+        response = self.client.login(
+            username='temporary', password='temporary')
+        self.assertEqual(response, True)
+
+    def test_not_logged_in(self):
+        self.client.logout()
+
+        response = self.client.get(
+            reverse('new-video'))
+        self.assertRedirects(response, '/login?next=/entry/videos/new')
+
+    def test_url_endpoint(self):
+        url = reverse('new-video')
+        self.assertEqual(url, '/entry/videos/new')
+
+    def test_form_fields(self):
+        """
+        Tests to see if the form contains all of the right fields
+        """
+        response = self.client.get(reverse('new-video'))
+
+        fields = {'video': 'input', 'caption': 'textarea'}
+        form = response.context['video_form']
+
+        for field in fields:
+            # for the Edit tests, you should be able to access
+            # form[field].value
+            self.assertIn(fields[field], str(form[field]))
+
+    def test_successful_video_creation_minimal(self):
+        """
+        POST a proper "new video" command to the server, and see if the
+        new video appears in the database. All optional fields are null.
+        """
+        Video.objects.all().delete()
+
+        # Data that we'll post to the server to get the new video created
+        new_video = {
+            'name': 'Fried', 'description': '', 'additional_info': ''}
+
+        self.client.post(reverse('new-video'), new_video)
+
+        video = Video.objects.all()[0]
+        for field in new_video:
+            self.assertEqual(
+                getattr(video, field), new_video[field])
+
+    def test_successful_video_creation_maximal(self):
+        """
+        POST a proper "new video" command to the server, and see if the
+        new video appears in the database. All optional fields are used.
+        """
+        Video.objects.all().delete()
+
+        # Data that we'll post to the server to get the new video created
+        new_video = {
+            'caption': "A thrilling display of utmost might",
+            'video': 'http://www.youtube.com/watch?v=dQw4w9WgXcQ'}
+
+        self.client.post(reverse('new-video'), new_video)
+
+        video = Video.objects.all()[0]
+        for field in new_video:
+            self.assertEqual(
+                getattr(video, field), new_video[field])
+
+    def test_no_data_error(self):
+        """
+        POST a "new video" command to the server missing all of the
+        required fields, and test to see what the error comes back as.
+        """
+        # Create a list of all objects before sending bad POST data
+        all_videos = Video.objects.all()
+
+        response = self.client.post(reverse('new-video'))
+        required_fields = ['video']
+        for field_name in required_fields:
+            self.assertIn(field_name,
+                          response.context['video_form'].errors)
+
+        # Test that we didn't add any new objects
+        self.assertEqual(
+            list(Video.objects.all()), list(all_videos))
