c4ec387f4e20a7b2c7daa04d7410f4bb764cfbf2
Evan Tschuy <tschuy@osuosl.org>
2014-11-21 15:40:37 -0800

    Bump Django to 1.7.1, fix pep8

--- whats_fresh/whats_fresh_api/signals.py
+++ whats_fresh/whats_fresh_api/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
 
+
 @receiver(post_save, sender=User)
 def default_group_callback(sender, instance, *args, **kwargs):
     if instance.groups.filter(name='Data Entry Users').exists():
