7f663a18819ac5e32431af75e0aeb24bcd03d6c8
Ken Lett <kennric@osuosl.org>
2014-11-21 10:36:08 -0800

    try simpler group creation

--- whats_fresh/whats_fresh_api/migrations/0002_auto_20141120_2246.py
+++ whats_fresh/whats_fresh_api/migrations/0002_auto_20141120_2246.py
@@ -5,17 +5,14 @@ from django.db import models, migrations
 from django.contrib.auth.models import Group
 
 def create_group(apps, schema_editor):
-    if Group.objects.filter(name='Data Entry Users').exists():
-        return
-    else:
-        group = Group.objects.create(name='Data Entry Users')
-        group.save()
-        return
+    Group.objects.get_or_create(name='Data Entry Users')
+    return
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('whats_fresh_api', '0001_initial'),
+        ('auth', '0001_initial')
     ]
 
     operations = [
