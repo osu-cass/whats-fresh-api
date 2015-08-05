0dca74ca992a27caf301e0a2be4c9ef7dfd49ed7
iCHAIT <cgupta319@gmail.com>
2015-08-06 01:32:15 +0530

    Site Title: from themes

---
+++ whats_fresh/whats_fresh_api/migrations/0008_auto_20150805_1958.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('whats_fresh_api', '0007_auto_20150804_1916'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='theme',
+            old_name='site_name',
+            new_name='site_title',
+        ),
+        migrations.RenameField(
+            model_name='theme',
+            old_name='site_name_slug',
+            new_name='site_title_slug',
+        ),
+    ]
--- whats_fresh/whats_fresh_api/models.py
+++ whats_fresh/whats_fresh_api/models.py
@@ -253,9 +253,9 @@ class Theme(models.Model):
     background_color = models.TextField(max_length=50)
     foreground_color = models.TextField(max_length=50)
     logo = models.ImageField(upload_to='images')
-    site_name = models.CharField(
+    site_title = models.CharField(
         max_length=50, default="Oregon's Catch", unique=True)
-    site_name_slug = models.SlugField(
+    site_title_slug = models.SlugField(
         max_length=40, default="Oregon's Catch", unique=True)
     vendors = models.CharField(max_length=100, default="vendors", unique=True)
     vendors_slug = models.SlugField(
