e584a9382c310347eaedc96f09437c54c98ff22e
iCHAIT <cgupta319@gmail.com>
2015-08-14 09:48:30 +0530

    models: default for non slugs titled

--- whats_fresh/whats_fresh_api/migrations/0004_theme.py
+++ whats_fresh/whats_fresh_api/migrations/0004_theme.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             name='Theme',
             fields=[
                 ('id', models.AutoField(
-                    verbose_name='ID', serialize=False,
-                    auto_created=True, primary_key=True)),
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=50)),
                 ('background_color', models.TextField(
                     default=b'rgb(81, 114, 133)', max_length=50)),
@@ -33,27 +33,27 @@ class Migration(migrations.Migration):
                 ('site_title', models.CharField(
                     default=b"Oregon's Catch", max_length=50)),
                 ('vendors', models.CharField(
-                    default=b'vendors', max_length=100)),
+                    default=b'Vendors', max_length=100)),
                 ('vendors_slug', models.SlugField(
                     default=b'vendors', max_length=40)),
                 ('products', models.CharField(
-                    default=b'products', max_length=100)),
+                    default=b'Products', max_length=100)),
                 ('products_slug', models.SlugField(
                     default=b'products', max_length=40)),
                 ('preparations', models.CharField(
-                    default=b'preparations', max_length=100)),
+                    default=b'Preparations', max_length=100)),
                 ('preparations_slug', models.SlugField(
                     default=b'preparations', max_length=40)),
                 ('stories', models.CharField(
-                    default=b'stories', max_length=100)),
+                    default=b'Stories', max_length=100)),
                 ('stories_slug', models.SlugField(
                     default=b'stories', max_length=40)),
                 ('videos', models.CharField(
-                    default=b'videos', max_length=100)),
+                    default=b'Videos', max_length=100)),
                 ('videos_slug', models.SlugField(
                     default=b'videos', max_length=40)),
                 ('images', models.CharField(
-                    default=b'images', max_length=100)),
+                    default=b'Images', max_length=100)),
                 ('images_slug', models.SlugField(
                     default=b'images', max_length=40)),
                 ('active', models.CharField(
